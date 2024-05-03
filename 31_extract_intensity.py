57f4d3b66e1c003a8fe16b494f407a76f4adf8ab
agerardin <antoine.gerardin@gmail.com>
2024-05-03 09:36:25 -0400

    fix: export format.

--- 31_extract_intensity.py
+++ 31_extract_intensity.py
@@ -2,6 +2,7 @@
 import string
 from itertools import product
 from pathlib import Path
+import numpy as np
 
 import os
 
@@ -11,6 +12,8 @@ from filepattern import FilePattern
 from lib import get_plate_params
 from lib import extract_intensity
 from lib import index_to_battleship
+
+import csv
 
 
 IMAGE_PATH = Path(".data/output/1.ome.tif")
@@ -28,18 +31,25 @@ print(params.Y)
 # and take that as center of a square around the well.
 # this is close to what the matlab code is doing
 R = min(params.X[1] - params.X[0], params.Y[1] - params.Y[0]) // 2
-R2 = ((params.X[1] - params.X[0]) // 2, (params.Y[1] - params.Y[0]) // 2) 
-print(R)
-print(R2)
 
+# This get us very close from the original values.
+R += 1
 fp = FilePattern(str(IMAGE_PATH.parent), "{index:d+}.ome.tif")
 
 temperature_range = [37, 90]
+temperatures = []
 
+with open("data/cleaned_expt1.csv", newline='\n') as csvfile:
+    results = csv.reader(csvfile, delimiter='\n')
+    for row in list(results)[1:]:
+        val = row[0].split(",")[0]
+        temperatures.append(val)
+
+print(temperatures)
 # Write a csv file
 with open(".data/output/plate.csv", "w") as fw:
 
-    headers = []
+    headers = ["Temperature"]
     for y, x in product(range(len(params.Y)),range(len(params.X))):
         headers.append(index_to_battleship(x, y, params.size))
     fw.write(",".join(headers) + "\n")
@@ -47,7 +57,7 @@ with open(".data/output/plate.csv", "w") as fw:
     for index, files in sorted(fp(pydantic_output=False), key=lambda x: x[0]['index']):
         with BioReader(files[0]) as br:
             image = br.read()
-            time_step = [
+            time_step = [temperatures[index['index']-1]] + [
                 extract_intensity(image, params.X[x], params.Y[y], R)
                 for y, x in product(range(len(params.Y)), range(len(params.X)))
             ]
