3064cb1f0f78729b4ca4e536208d82031e90413f
agerardin <antoine.gerardin@gmail.com>
2024-05-08 09:12:26 -0400

    dev: cannot install filepattern2 on debian.

--- rt_cetsa_analysis/run-plugin.sh
+++ rt_cetsa_analysis/run-plugin.sh
@@ -7,21 +7,22 @@ inpDir=./data
 outDir=./tmp
 container_input_dir="/inpDir"
 container_output_dir="/outDir"
-command="Rscript main.R"
-
-docker run -v $inpDir:/${container_input_dir} \
-            -v $outDir:/${container_output_dir} \
-            --user $(id -u):$(id -g) \
-            ${container_name}:${version} \
-            ${command} \
-            --params ${container_input_dir}/test_exp_param_full.csv \
-            --values ${container_input_dir}/test_exp_curve_all.csv \
-            --plate ${container_input_dir}/platemap.xlsx \
-            --outdir ${container_output_dir}
+# command="Rscript main.R"
+command="python3 __main__.py"
 
 # docker run -v $inpDir:/${container_input_dir} \
 #             -v $outDir:/${container_output_dir} \
 #             --user $(id -u):$(id -g) \
-#             --rm -it \
 #             ${container_name}:${version} \
-#             bash
+#             ${command} \
+#             --params ${container_input_dir}/test_exp_param_full.csv \
+#             --values ${container_input_dir}/test_exp_curve_all.csv \
+#             --plate ${container_input_dir}/platemap.xlsx \
+#             --outdir ${container_output_dir}
+
+docker run -v $inpDir:/${container_input_dir} \
+            -v $outDir:/${container_output_dir} \
+            --user $(id -u):$(id -g) \
+            --rm -it \
+            ${container_name}:${version} \
+            bash
